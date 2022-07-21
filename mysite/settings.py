b351e015bb2e2987b0e778c29639ec21e73c2271
AhmadHammad21 <ahmedhammad8765@gmail.com>
2022-07-21 16:56:57 +0300

    ya rab

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-w+b9weq7^d80@@=-#5n3v+gb*vhddbl@tk+-s8%kf62cs!ny2s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
